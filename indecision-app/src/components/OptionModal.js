2edcc79ccd9a677080fbb9b5e5d42c2da9c386fd
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-02-28 15:16:02 +0200

    styled react-modal

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -6,15 +6,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleDeleteSelectedOption}
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button 
-        onClick={(e) => {
-            props.handleDeleteSelectedOption(props.selectedOption)
-        }}
-        >
-            Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={(e) => {props.handleDeleteSelectedOption(props.selectedOption)}}>Okay</button>
     </Modal>
 );
 
