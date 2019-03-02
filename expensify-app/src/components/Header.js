a7de5322c158ff63ce54a40f8b0af2ce71669a55
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-03-02 15:04:37 +0200

    query strings and URL params

--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -4,13 +4,9 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>to Dashboard</NavLink>
-        <br/>
-        <NavLink to="/create" activeClassName="is-active">to Create Page</NavLink>
-        <br/>
-        <NavLink to="/edit" activeClassName="is-active">to Edit Page</NavLink>
-        <br/>
-        <NavLink to="/help" activeClassName="is-active">to Help Page</NavLink>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Page</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help Page</NavLink>
     </header>
 );
 
