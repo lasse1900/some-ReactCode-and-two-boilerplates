a68f5bd6ecd8193ddfc9be5b78c56e8f499194b2
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-03-03 18:10:51 +0200

    wrapping up Redux reducers

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -32,8 +32,11 @@ const resetCount = () => ({
     type: 'RESET'
 });
 
-const store = createStore((state = { count: 0}, action) => {
+// Reducers
+// 1. Reducers are pure functions
+// 2. Never change state or action
 
+const countReducer = (state = { count: 0}, action) => {
     switch (action.type) {
         case 'INCREMENT':
             // const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
@@ -57,7 +60,10 @@ const store = createStore((state = { count: 0}, action) => {
             return state;
     }
 
-});
+}
+
+
+const store = createStore(countReducer);
 
 const unsubsribe = store.subscribe(() => {
     console.log(store.getState());
