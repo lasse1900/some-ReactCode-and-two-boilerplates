88b22cfb8a98af39acca88896be3e6ecbeb865c1
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-03-03 10:58:36 +0200

    object and array ES& destructuring in playground folder

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,27 +1,44 @@
-const person = {
-    name: 'Lasse',
-    age: 51,
-    location: {
-        city: 'Vantaa',
-        temp: -4
-    } 
-};
+// Object destructuring 
 
-const { name: firstname = 'Anonymous', age } = person;
-console.log(`${firstname} is ${age}.`);
+// const person = {
+//     name: 'Lasse',
+//     age: 51,
+//     location: {
+//         city: 'Vantaa',
+//         temp: -4
+//     } 
+// };
 
-const { city, temp: temperature } = person.location;
-if(city && temperature) {
-    console.log(`It' s ${temperature} in ${city}.`);
-}
+// const { name: firstname = 'Anonymous', age } = person;
+// console.log(`${firstname} is ${age}.`);
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penquin'
-    }
-};
+// const { city, temp: temperature } = person.location;
+// if(city && temperature) {
+//     console.log(`It' s ${temperature} in ${city}.`);
+// }
 
-const { name: publisherName = 'Self-Published'} = book.publisher;
-console.log(publisherName);
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penquin'
+//     }
+// };
+
+// const { name: publisherName = 'Self-Published'} = book.publisher;
+// console.log(publisherName);
+
+// Array destructuring
+
+const address = ['08910 Tilhitie', 'Vantaa', 'Suomi', '61565'];
+const [, city, state = 'New York'] = address; 
+console.log(` You are in ${city} ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [item_name, price_200, price_250, price_275] = item;
+
+// console.log(`A medium Coffee (hot) costs $2.50`);
+console.log(`A medium ${item_name} costs ${price_250}.`)
+
+
