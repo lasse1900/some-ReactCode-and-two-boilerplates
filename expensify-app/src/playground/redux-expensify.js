f9b1145078f859e474d5f0b8746874953bd0c908
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-03-03 19:24:48 +0200

    sorting Redux data

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -137,6 +137,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
       const textMatch  = expense.description.toLowerCase().includes(text.toLowerCase());
 
     return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if(sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if(sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
     });
 };
 
@@ -154,7 +160,7 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -2000 }));
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
@@ -163,14 +169,14 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 30
 // store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter(''));
 
-// store.dispatch(sortByAmount()); // amount
+store.dispatch(sortByAmount()); // amount
 // store.dispatch(sortByDate());   // date
 
-store.dispatch(setStartDate(-1000));
+// store.dispatch(setStartDate(-1000));
 // store.dispatch(setStartDate());
-store.dispatch(setEndDate(1250));
+// store.dispatch(setEndDate(1250));
 
-store.dispatch(setTextFilter('cof'));
+// store.dispatch(setTextFilter('cof'));
 
 const demoState = {
     expenses: [{
