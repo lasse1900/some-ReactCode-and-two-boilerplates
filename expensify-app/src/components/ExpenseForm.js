5b639e6fa76487d5bb0aac20ddb0c902795b03e8
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-03-06 11:57:32 +0200

    setup of DatePicker (momentjs.com)

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+// const date = new Date();
+const now = moment();
+console.log(now.format("Do MMM, YYYY"));
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        caledarFocused: false
     };
     onDescriptionChange = (e) => {
         const description = e.target.value;
@@ -19,6 +28,12 @@ export default class ExpenseForm extends React.Component {
         if (amount.match(/^\d*(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }));
         }
+    };
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }));
+    };
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ( { caledarFocused: focused}));
     };
 
     render() {
@@ -37,6 +52,14 @@ export default class ExpenseForm extends React.Component {
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker 
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.caledarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false }
                     />
                     <textarea
                         type="text"
