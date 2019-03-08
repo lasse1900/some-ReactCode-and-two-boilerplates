f231e400aa496b7772843724e72ce15a056d9543
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-03-08 12:58:47 +0200

    minor naming changes to AddExpensePage.js and test

--- expensify-app/src/components/AddExpensePage.js
+++ expensify-app/src/components/AddExpensePage.js
@@ -4,26 +4,24 @@ import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
-    onSubmit = (expense) => {
-        this.props.onSubmit(expense);
-        this.props.history.push('/');
-    };
-    render() {
-        return (
-            <div>
-            <h1>Add Expense</h1>
-            <ExpenseForm 
-                onSubmit={this.onSubmit}
-            />
-        </div>
-        );
-    }
+  onSubmit = (expense) => {
+    this.props.addExpense(expense);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onSubmit: (expense) => dispatch(addExpense(expense))
-    };
-};
+const mapDispatchToProps = (dispatch) => ({
+  addExpense: (expense) => dispatch(addExpense(expense))
+});
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
--- expensify-app/src/tests/components/AddExpensePage.test.js
+++ expensify-app/src/tests/components/AddExpensePage.test.js
@@ -3,20 +3,20 @@ import { shallow } from 'enzyme';
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
-let onSubmit, history, wrapper;
+let addExpense, history, wrapper;
 
 beforeEach(() => {
-    onSubmit = jest.fn();
-    history = {push: jest.fn() };
-    wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history}/>);
+  addExpense = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
 });
 
-test('should render AddExpensePage corretly', () => {
-    expect(wrapper).toMatchSnapshot();
+test('should render AddExpensePage correctly', () => {
+  expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle onSubmit', () => {
-    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
-    expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
